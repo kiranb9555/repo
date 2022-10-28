8228b54d00dd7080f3fc8373a9a78d0e43957344
kiran <kiranb9555@gmail.com>
2022-10-28 15:48:49 +0530

    update db with every new api data

--- Router.js
+++ Router.js
@@ -24,6 +24,87 @@ export default class Router extends Component {
       searchText: '',
       slideAnim: new Animated.Value(0)
     };
+    var obj =[
+      {
+          "id": 4194,
+          "name": "Bhoopati Varman I",
+          "email": "i_varman_bhoopati@buckridge-jones.com",
+          "gender": "female",
+          "status": "active"
+      },
+      {
+          "id": 4193,
+          "name": "Gautam Rana II",
+          "email": "rana_ii_gautam@funk.biz",
+          "gender": "female",
+          "status": "active"
+      },
+      {
+          "id": 4192,
+          "name": "Pran Mehra",
+          "email": "mehra_pran@leannon-spinka.org",
+          "gender": "male",
+          "status": "inactive"
+      },
+      {
+          "id": 4191,
+          "name": "Paramartha Mukhopadhyay",
+          "email": "mukhopadhyay_paramartha@beier.name",
+          "gender": "male",
+          "status": "inactive"
+      },
+      {
+          "id": 4190,
+          "name": "Sudeva Dhawan",
+          "email": "sudeva_dhawan@rutherford.name",
+          "gender": "male",
+          "status": "inactive"
+      },
+      {
+          "id": 4189,
+          "name": "Chakravartee Ganaka",
+          "email": "chakravartee_ganaka@beahan-shields.name",
+          "gender": "female",
+          "status": "inactive"
+      },
+      {
+          "id": 4188,
+          "name": "Gov. Sujata Singh",
+          "email": "singh_gov_sujata@kovacek.net",
+          "gender": "female",
+          "status": "inactive"
+      },
+      {
+          "id": 4187,
+          "name": "Mohan Patel",
+          "email": "patel_mohan@fay-dach.com",
+          "gender": "male",
+          "status": "active"
+      },
+      {
+          "id": 4186,
+          "name": "Trilokesh Trivedi",
+          "email": "trilokesh_trivedi@keeling-beer.org",
+          "gender": "male",
+          "status": "inactive"
+      },
+      {
+          "id": 4185,
+          "name": "Chidananda Nair",
+          "email": "chidananda_nair@fadel.co",
+          "gender": "female",
+          "status": "active"
+      },
+      {
+        "id": 4184,
+        "name": "yadav Nair",
+        "email": "yadav Nair@fadel.co",
+        "gender": "male",
+        "status": "active"
+    }
+  ]
+      
+    
   }
 
   componentDidMount() {
@@ -35,10 +116,11 @@ export default class Router extends Component {
   ExecuteQuery = (sql, params = []) => new Promise((resolve, reject) => {
     this.db.transaction((trans) => {
       trans.executeSql(sql, params, (trans, results) => {
+       console.log("executed sql______%s__%s",params,sql) 
         resolve(results);
       },
         (error) => {
-          reject(error);
+          reject("error__%s__%s",error,sql);
         });
     });
   });
@@ -68,7 +150,18 @@ export default class Router extends Component {
   addDataInDataBase = async (data) => {
     for (let i = 0; i < data.length; ++i) {
       let item = data[i];
+      let Deletetable = await this.ExecuteQuery("DELETE FROM users",[]);
+      
       let singleInsert = await this.ExecuteQuery("INSERT INTO users (id, email, gender, name, status) VALUES ( ?, ?, ?, ?, ? )", [item.id, item.email, item.gender, item.name, item.status]);
+      
+      let data = await this.ExecuteQuery('SELECT * FROM users', []);
+       var rows = data.rows;
+       let fullData = [];
+     
+      for (let i = 0; i < rows.length; i++) {
+         var itemsave = rows.item(i);
+        fullData.push(itemsave);
+       }
     }
     // // data?.forEach(item => {
     // //   let singleInsert = await this.ExecuteQuery("INSERT INTO users (id, email, gender, name, status) VALUES ( ?, ?, ?, ?, ? )", [item.id, item.email, item.gender, item.name, item.status]);
@@ -108,7 +201,9 @@ export default class Router extends Component {
     let fullData = [];
     for (let i = 0; i < rows.length; i++) {
       var item = rows.item(i);
+      console.log("item from row item(i)_____________", item);
       fullData.push(item);
+      console.log(this.state.fullData);
     }
 
     if (fullData?.length > 0) {
@@ -120,21 +215,23 @@ export default class Router extends Component {
   addData = async (resJson) => {
     // let data = Array(20).fill(1).map((item, index) => index+1);
     // data.forEach(item => this.DeleteQuery(item))
-
-    let data = await this.ExecuteQuery('SELECT * FROM users', []);
-    var rows = data.rows;
-    let fullData = [];
-    for (let i = 0; i < rows.length; i++) {
-      var item = rows.item(i);
-      fullData.push(item);
-    }
+    this.addDataInDataBase(resJson);
+    // let data = await this.ExecuteQuery('SELECT * FROM users', []);
+    // var rows = data.rows;
+    // let fullData = [];
+   
+    // for (let i = 0; i < rows.length; i++) {
+    //   var item = rows.item(i);
+    //   fullData.push(item);
+    // }
     console.log('fullData', fullData);
     if (fullData?.length > 0) {
       this.setState({ data: fullData, searchedDataList: fullData, searchedDataGrid: fullData, searchedDataListandGrid: fullData });
       return false;
-    } else {
-      this.addDataInDataBase(resJson);
-    }
+     } else {
+    //  this.addDataInDataBase(resJson);
+console.log("error");
+     }
   }
 
   fetchCats() {
@@ -143,7 +240,7 @@ export default class Router extends Component {
       .then(res => res.json())
       .then(resJson => {
         this.setState({ data: resJson, searchedDataList: resJson, searchedDataGrid: resJson, searchedDataListandGrid: resJson, refreshing: false });
-        resJson?.length > 0 && this.addData(resJson)
+        resJson?.length > 0 && this.addData(obj)
       })
       .catch(e => {
         this.getData();
